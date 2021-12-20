c286c5f1d3f12f81bd015879628f9fc5f9c3e696
E-SeongHo <lsh031255@naver.com>
2021-12-20 21:13:48 +0900

    implement : ray.h

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Toy Ray Tracer
+
+study log : Peter Shirley's Ray Tracing In One Weekend series.
---
+++ build/CMakeFiles/RayTracer.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RayTracer.dir/src/main.cpp.obj"
+  "CMakeFiles/RayTracer.dir/src/main.cpp.obj.d"
+  "RayTracer.exe"
+  "RayTracer.exe.manifest"
+  "RayTracer.pdb"
+  "libRayTracer.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RayTracer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
