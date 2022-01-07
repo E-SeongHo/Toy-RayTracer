b6434f437162eb5cf3b93180672c549d7d79cb67
E-SeongHo <lsh031255@naver.com>
2022-01-08 02:54:46 +0900

    computes the color according to its surface normal

--- build/CMakeFiles/RayTracer.dir/DependInfo.cmake
+++ build/CMakeFiles/RayTracer.dir/DependInfo.cmake
@@ -1,15 +1,14 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
-  "CXX"
   )
-# The set of files for implicit dependencies of each language:
-set(CMAKE_DEPENDS_CHECK_CXX
-  "C:/Codes/Toy-RayTracer/src/main.cpp" "C:/Codes/Toy-RayTracer/build/CMakeFiles/RayTracer.dir/src/main.cpp.obj"
-  )
-set(CMAKE_CXX_COMPILER_ID "GNU")
 
-# The include file search paths:
-set(CMAKE_CXX_TARGET_INCLUDE_PATH
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Workspace/ToyProject/Toy-RayTracer/src/main.cpp" "CMakeFiles/RayTracer.dir/src/main.cpp.obj" "gcc" "CMakeFiles/RayTracer.dir/src/main.cpp.obj.d"
   )
 
 # Targets to which this target links.
