8d69c8659a376a5b23cef9e3c17c230702965ff3
E-SeongHo <lsh031255@naver.com>
2022-01-08 20:38:16 +0900

    anti-aliasing

--- src/includes/common.h
+++ src/includes/common.h
@@ -4,7 +4,7 @@
 #include <cmath>
 #include <limits>
 #include <memory>
-
+#include <random>
 
 // Usings
 
@@ -19,8 +19,25 @@ const double pi = 3.1415926535897932385;
 
 // Utility Functions
 
-inline double degrees_to_radians(double degrees) {
+inline double degrees_to_radians(double degrees) 
+{
     return degrees * pi / 180.0;
+}
+inline double random_double()
+{
+    static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+    static std::mt19937 generator;
+    return distribution(generator);
+}
+inline double random_double(double min, double max)
+{
+    return min + (max-min)*random_double();
+}
+inline double clamp(double x, double min, double max)
+{
+    if (x < min) return min;
+    if (x > max) return max;
+    return x;
 }
 
 // Common Headers
