758550544e87f7944e280dd4ba4c81b9069cf66e
E-SeongHo <lsh031255@naver.com>
2022-01-14 01:09:18 +0900

    render diffuse materials and gamma correction

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,15 @@
 #include "includes\sphere.h"
 #include "includes\camera.h"
 
-color ray_color(const ray& r, const hittable& world)
+color ray_color(const ray& r, const hittable& world, int depth)
 {
     hit_record rec;
-    if (world.hit(r, 0, infinity, rec)) 
-        return 0.5 * (rec.normal + color(1,1,1));
+    if (depth <= 0) return color(0,0,0);
+    if (world.hit(r, 0, infinity, rec))
+    {
+        point3 target = rec.p + rec.normal + random_in_unit_sphere();
+        return 0.5 * ray_color(ray(rec.p, target - rec.p), world, depth-1);
+    } 
     vec3 unit_direction = unit_vector(r.direction());
     auto t = 0.5*(unit_direction.y() + 1.0);
     return (1.0 - t)*color(1.0,1.0,1.0) + t*color(0.5,0.7,1.0);
@@ -22,6 +26,7 @@ int main()
     const int image_width = 400;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
     const int samples_per_pixel = 100;
+    const int max_depth = 50;
 
     // World
     hittable_list world;
@@ -45,7 +50,7 @@ int main()
                 auto u = (i + random_double()) / (image_width-1);
                 auto v = (j + random_double()) / (image_height-1);
                 ray r = cam.get_ray(u, v);
-                pixel_color += ray_color(r,world);
+                pixel_color += ray_color(r, world, max_depth);
             }
             write_color(std::cout, pixel_color, samples_per_pixel);
         }
