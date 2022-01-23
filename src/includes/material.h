829f71c8253695bcfe33dd797d706e17a8bdc587
E-SeongHo <lsh031255@naver.com>
2022-01-23 22:02:07 +0900

    implement refraction, dielectric material

--- src/includes/material.h
+++ src/includes/material.h
@@ -2,7 +2,10 @@
 #define MATERIAL_H
 
 #include "common.h"
-#include "hittable.h"
+//#include "hittable.h"
+#include "vec3.h"
+
+struct hit_record;
 
 class material
 {
@@ -37,19 +40,54 @@ class metal : public material
 {
 public:
     color albedo;
+    double fuzz;
 
 public:
-    metal(const color& a) : albedo(a) {}
+    metal(const color& a, const double f) : albedo(a), fuzz(f < 1 ? f : 1) {}
 
     virtual bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered) const override
     {
         vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
-        scattered = ray(rec.p, reflected);
+        scattered = ray(rec.p, reflected + fuzz*random_in_unit_sphere());
         attenuation = albedo;
         return (dot(scattered.direction(), rec.normal) > 0);
     }
 };
 
+class dielectric : public material
+{
+public:
+    double ir;
 
+private:
+    static double reflectance(double cosine, double ref_idx)
+    {   // Use Schlick's approximation for reflectance.
+        auto r0 = (1-ref_idx) / (1+ref_idx);
+        r0 = r0 * r0;
+        return r0 + (1-r0)*pow((1-cosine), 5);
+    }
+public:
+    dielectric(double index_of_refraction) : ir(index_of_refraction) {}
+    virtual bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered) const override
+    {
+        attenuation = color(1.0, 1.0, 1.0);
+        double refraction_ratio = rec.front_face ? (1.0/ir) : ir;
+
+        vec3 unit_direction = unit_vector(r_in.direction());
+        double cos_theta = fmin(dot(-unit_direction, rec.normal), 1.0);
+        double sin_theta = sqrt(1.0 - cos_theta*cos_theta);
+
+        bool cannot_refract = refraction_ratio * sin_theta > 1.0; // Total Internal Reflection
+        vec3 direction;
+
+        if (cannot_refract || reflectance(cos_theta, refraction_ratio) > random_double()) 
+            direction = reflect(unit_direction, rec.normal);
+        else 
+            direction = refract(unit_direction, rec.normal, refraction_ratio);
+
+        scattered = ray(rec.p, direction);
+        return true;
+    }
+};
 
 #endif
--- src/includes/vec3.h
+++ src/includes/vec3.h
@@ -23,7 +23,7 @@ public:
         const auto s = 1e-8;
         return ( (fabs(e[0] < s)) && (fabs(e[1] < s)) && (fabs(e[2] < s)) );
     }
-    vec3 operator-() const { return vec3(e[0], -e[1], -e[2]); }
+    vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
     double operator[](int i) const { return e[i]; }
     double& operator[](int i) { return e[i]; }
 
@@ -141,5 +141,13 @@ vec3 reflect(const vec3& v, const vec3& n)
 {
     return v - 2*dot(v,n)*n;
 }
+vec3 refract(const vec3 &uv, const vec3 &n, double etai_over_etat)
+{
+    auto cos_theta = fmin(dot(-uv, n), 1.0);
+    vec3 r_out_perp = etai_over_etat * (uv + cos_theta*n);
+    vec3 r_out_parallel = -sqrt(fabs(1.0 - r_out_perp.length_squared())) * n;
+    return r_out_perp + r_out_parallel;
+}
 
+    
 #endif 
